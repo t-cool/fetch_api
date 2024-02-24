5016f79f06cd69294d6449d13a54081754197cd0
t-cool <hiroki802@hotmail.com>
2024-02-24 09:27:05 +0900

    tweak

--- src/fetch/fetch_post.js
+++ src/fetch/fetch_post.js
@@ -13,22 +13,3 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error('Error:', error));
-
-// PUT リクエスト
-fetch('https://jsonplaceholder.typicode.com/posts/1', {
-  method: 'PUT',
-  headers: {
-    // 'Content-Type' ヘッダーは、リクエストの本体がどのような形式であるかを示します。
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    id: 1,
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
-
